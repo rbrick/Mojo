bd14134a6874da4b34107bbbe7ea495d5e86c698
rbrick <rbrickproductions123@gmail.com>
2015-02-03 21:26:44 -0600

    Remove Plugin related shiz

--- src/org/nebulamc/ncommand/command/parameter/ParametricRegistry.java
+++ src/org/nebulamc/ncommand/command/parameter/ParametricRegistry.java
@@ -22,4 +22,5 @@ public class ParametricRegistry {
         }
         return null;
     }
+
 }
--- src/org/nebulamc/ncommand/nCommand.java
+++ src/org/nebulamc/ncommand/nCommand.java
@@ -1,20 +1,10 @@
 package org.nebulamc.ncommand;
 
-import org.nebulamc.ncommand.annotations.Command;
-import org.nebulamc.ncommand.annotations.Permission;
-
 /**
  * Created by Ryan on 1/29/2015
  * <p/>
  * Project: nCommand
  */
 public class nCommand {
-
-
-    @Command(name="example", aliases = {"ex"})
-    @Permission("example.*")
-    public void example(String str) {
-    }
-
-
+    // This is a dummy class. huehuehue
 }
